330c8dd957ec6dc9174d48d2f5607110fbda47e5
triplex6 <you@example.com>
2023-04-26 16:47:47 +0200

    add table chanson

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,9 +1,20 @@
 --table chanteur (Oracle DB format)
 
-create table chanteur (
-			id INTEGER
-				GENERATED ALWAYS AS IDENTITY
-				CONSTRAINT pk_chanteur PRIMARY KEY,
-		nom VARCHAR2(50) NOT NULL,
-		birthdate DATE
-)
+CREATE TABLE chanteur (
+	id INTEGER
+		GENERATED ALWAYS AS IDENTITY
+		CONSTRAINT pk_chanteur PRIMARY KEY,
+	nom VARCHAR2(50) NOT NULL,
+	birthdate DATE
+);
+
+CREATE TABLE chanson (
+	id INTEGER
+		GENERATED ALWAYS AS IDENTITY
+		CONSTRAINT pk_chanson PRIMARY KEY,
+	title VARCHAR2(150) NOT NULL,
+	id_chanteur INTEGER
+);
+
+ALTER TABLE chanson add CONSTRAINT FOREIGN KEY id_chanteur
+	REFERENCES chanteur(id);
